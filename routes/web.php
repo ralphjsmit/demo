0af6328c7944ccd352dbc2193f4297e5c434751b
Ralph J. Smit <59207045+ralphjsmit@users.noreply.github.com>
2024-12-27 16:07:17 +0100

    Add opcache UI

--- routes/web.php
+++ routes/web.php
@@ -3,3 +3,7 @@
 use App\Livewire\Form;
 
 \Illuminate\Support\Facades\Route::get('form', Form::class);
+
+\Illuminate\Support\Facades\Route::get('opcache', function () {
+    require_once __DIR__ . '/../vendor/amnuts/opcache-gui/index.php';
+});
